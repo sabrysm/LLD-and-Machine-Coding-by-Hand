3755cb91bbb61158429b1d13726cdd2a65d9c00d
Abdelrahman Sabry <drakth5364@gmail.com>
2026-09-13 16:05:41 +0300

    Add properties to the config attributes and fix encapsulation

--- ratelimiter/entities/rate_limiter_config.py
+++ ratelimiter/entities/rate_limiter_config.py
@@ -1,4 +1,12 @@
 class RateLimiterConfig:
     def __init__(self, max_requests: int, window_in_seconds: int):
-        self.max_requests = max_requests
-        self.window_in_seconds = window_in_seconds
+        self._max_requests = max_requests
+        self._window_in_seconds = window_in_seconds
+    
+    @property
+    def max_requests(self):
+        return self._max_requests
+    
+    @property
+    def window_in_seconds(self):
+        return self._window_in_seconds
