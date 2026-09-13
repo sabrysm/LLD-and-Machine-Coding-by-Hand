eada1022a2fb26b8e20ed15d4b61ad54d8c37037
Abdelrahman Sabry <drakth5364@gmail.com>
2026-09-13 16:11:49 +0300

    feat(ratelimiter): Add setters for the config

--- ratelimiter/entities/rate_limiter_config.py
+++ ratelimiter/entities/rate_limiter_config.py
@@ -7,6 +7,18 @@ class RateLimiterConfig:
     def max_requests(self):
         return self._max_requests
     
+    @property.setter
+    def max_requests(self, value):
+        if not isinstance(value, int):
+            raise TypeError("max_requests must be Integer")
+        self._max_requests = value
+    
     @property
     def window_in_seconds(self):
         return self._window_in_seconds
+    
+    @property.setter
+    def window_in_seconds(self, value):
+        if not isinstance(value, int):
+            raise TypeError("window_in_seconds must be Integer")
+        self._window_in_seconds = value
